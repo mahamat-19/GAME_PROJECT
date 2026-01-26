d28fb925c9d1b7f38bd166678e8beeabda97e003
sanalinker <djouma@063gmail.com>
2026-01-26 02:28:49 +0100

    color mode failed

--- GameApp/src/main.tsx
+++ GameApp/src/main.tsx
@@ -1,12 +1,12 @@
-import { Provider } from "./components/ui/provider.tsx"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Provider } from "./components/ui/provider.tsx"
 import { App } from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+      <Provider>
+        <App />
+      </Provider>
+    </React.StrictMode>
+);
